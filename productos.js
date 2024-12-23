89435a6eee0d8e247743dd8bae15a8963ca6d308
tomas-gabioud <87837311+tomas-gabioud@users.noreply.github.com>
2024-12-22 22:39:42 -0300

    corrección url en el fetch productos.js

--- productos.js
+++ productos.js
@@ -1,5 +1,5 @@
 // Consumimos los productos del JSON.
-fetch()
+fetch(https://tomas-gabioud.github.io/talento-tech/productos.json)
     .then(response => {
         if (!response.ok) {
             throw new Error('Error al cargar el archivo JSON');
